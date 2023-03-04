f6ea7c209b595bacc549bb516055fc8d8c656266
Stevie <75964167+Stevie-Bland@users.noreply.github.com>
2023-03-04 16:19:25 -0600

    Uploaded ALL Files
    Completed the project. 
    
    May add my own styling.

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Steven on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
